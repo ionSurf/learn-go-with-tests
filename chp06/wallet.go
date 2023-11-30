38f1e116368772ef20feb03cdd58bea016d2f0c7
Javier Prieto <javier.accounts@fiml.co>
2023-11-30 18:56:02 +0000

    Wraps up chapter 6

--- chp06/wallet.go
+++ chp06/wallet.go
@@ -1,6 +1,11 @@
 package wallet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("widthrawl amount is higher than the wallet's balance")
 
 type Stringer interface {
 	String() string
@@ -24,6 +29,10 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Widthraw(amount Bitcoin) {
+func (w *Wallet) Widthraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
 	w.balance -= amount
+	return nil
 }
--- chp06/wallet_test.go
+++ chp06/wallet_test.go
@@ -5,26 +5,48 @@ import "testing"
 func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(Bitcoin(10))
-		result := wallet.Balance()
-		expected := Bitcoin(10.0)
-
-		if result != expected {
-			t.Errorf("%v expected %s result %s", wallet, expected, result)
-		}
+		assertBalance(t, wallet, Bitcoin(10.0))
 	})
 	t.Run("Widthraw", func(t *testing.T) {
 		wallet := Wallet{
-			balance: 10.0,
+			balance: Bitcoin(10.0),
 		}
-
-		wallet.Widthraw(Bitcoin(5))
-		result := wallet.Balance()
-		expected := Bitcoin(5.0)
-
-		if result != expected {
-			t.Errorf("%v expected %s result %s", wallet, expected, result)
-		}
+		err := wallet.Widthraw(Bitcoin(5))
+		assertBalance(t, wallet, Bitcoin(5.0))
+		assertNotErr(t, err)
 	})
+	t.Run("Widthraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(10.0)
+		wallet := Wallet{
+			balance: startingBalance,
+		}
+		err := wallet.Widthraw(Bitcoin(15.0))
+		expectedErrString := ErrInsufficientFunds
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, expectedErrString)
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, expected Bitcoin) {
+	t.Helper()
+	if wallet.Balance() != expected {
+		t.Errorf("Expected %s, result %s", expected, wallet.balance)
+	}
+}
+func assertNotErr(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("Not execting an error")
+	}
+}
+func assertError(t testing.TB, resultErr error, expectedErr error) {
+	t.Helper()
+	if resultErr == nil {
+		t.Fatal("Expected an error to come through")
+	}
+	if resultErr != expectedErr {
+		t.Errorf("Expected error %q received %q", expectedErr, resultErr)
+	}
 }
